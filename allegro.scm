f6273f492923a494e85563e3a3cbd9caee58b1ac
Dan Leslie <dan@ironoxide.ca>
2012-01-28 22:53:22 -0800

    First commit

---
+++ allegro.scm
@@ -0,0 +1,8 @@
+#>
+#include <allegro5/allegro.h>
+<#
+
+(module allegro *
+        (import scheme chicken bind foreign)
+        (include "base")
+)
---
+++ base.scm
@@ -0,0 +1,32 @@
+(define-record version
+  major sub wip release-number string date-string date int)
+
+(define allegro-version 
+  (make-version 
+   (foreign-value "ALLEGRO_VERSION" integer)
+   (foreign-value "ALLEGRO_SUB_VERSION" integer)
+   (foreign-value "ALLEGRO_WIP_VERSION" integer)
+   (foreign-value "ALLEGRO_RELEASE_NUMBER" integer)
+   (foreign-value "ALLEGRO_VERSION_STR" c-string)
+   (foreign-value "ALLEGRO_DATE_STR" c-string)
+   (foreign-value "ALLEGRO_DATE" integer)
+   (foreign-value "ALLEGRO_VERSION_INT" unsigned-integer32)))
+
+(define pi (foreign-value "ALLEGRO_PI" integer32))
+
+(define make-id (foreign-lambda* unsigned-integer32 ((unsigned-integer32 a) (unsigned-integer32 b) (unsigned-integer32 c) (unsigned-integer32 d)) "C_return(AL_ID(a, b, c, d));"))
+
+(bind-rename _AL_DRIVER_INFO driver-info)
+(bind* #<<ENDC
+#ifdef CHICKEN
+typedef struct _AL_DRIVER_INFO      /* info about a hardware driver */
+{
+   int id;                          /* integer ID */
+   void *driver;                    /* the driver structure */
+   int autodetect;                  /* set to allow autodetection */
+} _AL_DRIVER_INFO;
+#endif
+ENDC
+)
+
+(define run-main (foreign-safe-lambda int "al_run_main" integer (c-pointer c-string) c-pointer))
