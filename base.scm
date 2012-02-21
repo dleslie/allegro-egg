c98e20de1ad4c1dfbf78e7f6b039d4360f4474e3
Dan Leslie <dan@ironoxide.ca>
2012-02-21 07:07:35 -0800

    Began reorganization, first pass on base

--- base.scm
+++ base.scm
@@ -1,20 +1,9 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Types
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (define-record version
   major sub wip release-number string date-string date int)
-
-(define allegro-version 
-  (make-version 
-   (foreign-value "ALLEGRO_VERSION" integer)
-   (foreign-value "ALLEGRO_SUB_VERSION" integer)
-   (foreign-value "ALLEGRO_WIP_VERSION" integer)
-   (foreign-value "ALLEGRO_RELEASE_NUMBER" integer)
-   (foreign-value "ALLEGRO_VERSION_STR" c-string)
-   (foreign-value "ALLEGRO_DATE_STR" c-string)
-   (foreign-value "ALLEGRO_DATE" integer)
-   (foreign-value "ALLEGRO_VERSION_INT" unsigned-integer32)))
-
-(define pi (foreign-value "ALLEGRO_PI" integer32))
-
-(define make-id (foreign-lambda* unsigned-integer32 ((unsigned-integer32 a) (unsigned-integer32 b) (unsigned-integer32 c) (unsigned-integer32 d)) "C_return(AL_ID(a, b, c, d));"))
 
 (bind-rename _AL_DRIVER_INFO driver-info)
 (bind* #<<ENDC
@@ -29,5 +18,23 @@ typedef struct _AL_DRIVER_INFO      /* info about a hardware driver */
 ENDC
 )
 
-(define run-main (foreign-safe-lambda int "al_run_main" integer (c-pointer c-string) (function integer (integer (c-pointer c-string)))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Methods
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(define base/version 
+  (make-version 
+   (foreign-value "ALLEGRO_VERSION" integer)
+   (foreign-value "ALLEGRO_SUB_VERSION" integer)
+   (foreign-value "ALLEGRO_WIP_VERSION" integer)
+   (foreign-value "ALLEGRO_RELEASE_NUMBER" integer)
+   (foreign-value "ALLEGRO_VERSION_STR" c-string)
+   (foreign-value "ALLEGRO_DATE_STR" c-string)
+   (foreign-value "ALLEGRO_DATE" integer)
+   (foreign-value "ALLEGRO_VERSION_INT" unsigned-integer32)))
+
+(define base/pi (foreign-value "ALLEGRO_PI" integer32))
+
+(define base/make-id (foreign-lambda* unsigned-integer32 ((unsigned-integer32 a) (unsigned-integer32 b) (unsigned-integer32 c) (unsigned-integer32 d)) "C_return(AL_ID(a, b, c, d));"))
+
+(define base/run-main (foreign-safe-lambda int "al_run_main" integer (c-pointer c-string) (function integer (integer (c-pointer c-string)))))
