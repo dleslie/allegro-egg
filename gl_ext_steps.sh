bbf1707c2368fe6f14ef8de52c9fd6ef6714ccb8
Dan Leslie <dan@ironoxide.ca>
2024-06-23 08:49:02 -0700

    More parsed

--- gl_ext_steps.sh
+++ gl_ext_steps.sh
@@ -9,7 +9,11 @@
  cat /c/Users/dan/OneDrive/bin/w64devkit/include/allegro5/opengl/GLext/gl_ext_defs.h \
      | grep -v typedef;
  gcc -DAPIENTRY= -DWINGDIAPI= -Istub -nostdinc -P -E \
+     -I/c/Users/dan/OneDrive/bin/w64devkit/x86_64-w64-mingw32/include/ \
      /c/Users/dan/OneDrive/bin/w64devkit/x86_64-w64-mingw32/include/GL/gl.h;
+ gcc -DAPIENTRY= -DWINGDIAPI= -Istub -nostdinc -P -E \
+     -I/c/Users/dan/OneDrive/bin/w64devkit/x86_64-w64-mingw32/include/ \
+     /c/Users/dan/OneDrive/bin/w64devkit/x86_64-w64-mingw32/include/GL/glu.h;
  gcc -DALLEGRO_UNSTABLE -D"AL_VAR(type, name) extern type name" \
      -nostdinc -P -E -Istub \
      /c/Users/dan/OneDrive/bin/w64devkit/include/allegro5/opengl/gl_ext.h) \
@@ -19,6 +23,7 @@
 | sed -e "s#long long#int64_t#g" \
 | sed -e "s#unsigned int64_t#uint64_t#g" \
 | sed -e "s#\(^typedef *\([^(]*\)( *\\* *\([^)]*\))\(.*\);\)#\2\3\4;#g" \
+| sed -e "s#(CALLBACK#(#g" \
 | grep -v PF \
 | grep -v "\\[" \
 | grep -v "extern _ALLEGRO_" \
@@ -31,11 +36,11 @@
 | chicken-bind \
     -o gl.scm \
     -export-constants \
-    -rename-regex "_ALLEGRO_gl(.*)_t":"\1" \
-    -rename-regex "(ALLEGRO_OGL_EXT_API-)":"ext-api:" \
-    -rename-regex "(ALLEGRO_OGL_EXT_LIST-)":"ext-list:" \
-    -rename-regex "ALLEGRO_GL_(.*)":"\1" \
-    -rename-regex "^GL_?(.*)$":"\1" \
-    -rename-regex "^gl(.*)$":"\1" \
+    -rename-regex "_ALLEGRO_gl(.*)_t":"gl:\1" \
+    -rename-regex "ALLEGRO_OGL_EXT_API-(.*)":"gl-ext-api:\1" \
+    -rename-regex "ALLEGRO_OGL_EXT_LIST-(.*)":"gl-ext-list:\1" \
+    -rename-regex "ALLEGRO_GL_(.*)":"gl:\1" \
+    -rename-regex "^GL_?(.*)$":"gl:\1" \
+    -rename-regex "^gl(.*)$":"gl:\1" \
     -default-renaming "" \
 	  -
